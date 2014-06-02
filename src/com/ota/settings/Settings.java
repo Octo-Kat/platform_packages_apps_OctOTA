3228ad05d70003c20da8432f85f347c86e300b2c
Don Kaczmarek <cjkacz@gmail.com>
2014-06-02 14:51:57 -0400

    OTA Interval Checks

--- src/com/ota/settings/Settings.java
+++ src/com/ota/settings/Settings.java
@@ -79,13 +79,13 @@ public class Settings extends PreferenceActivity implements
             boolean enableUpdateCheck = true;
             switch(interval) {
                 case 0:
-                    UpdateListener.interval = AlarmManager.INTERVAL_DAY;
+                    UpdateListener.interval = AlarmManager.INTERVAL_DAY * 7;
                     break;
                 case 1:
-                    UpdateListener.interval = AlarmManager.INTERVAL_HALF_DAY;
+                    UpdateListener.interval = AlarmManager.INTERVAL_DAY;
                     break;
                 case 2:
-                    UpdateListener.interval = AlarmManager.INTERVAL_HOUR;
+                    UpdateListener.interval = AlarmManager.INTERVAL_HALF_DAY;
                     break;
                 case 3:
                     enableUpdateCheck = false;
@@ -99,7 +99,7 @@ public class Settings extends PreferenceActivity implements
                 WakefulIntentService.scheduleAlarms(new UpdateListener(), this, false);
             } else {
                 SharedPreferences prefs = getSharedPreferences(LAST_INTERVAL, 0);
-                prefs.edit().putLong(LAST_INTERVAL, 1).apply();
+                prefs.edit().putLong(LAST_INTERVAL, 0).apply();
                 WakefulIntentService.cancelAlarms(this);
             }
     }
@@ -108,11 +108,11 @@ public class Settings extends PreferenceActivity implements
         SharedPreferences prefs = getSharedPreferences(LAST_INTERVAL, 0);
         long value = prefs.getLong(LAST_INTERVAL,0);
         int settingsValue;
-        if (value == AlarmManager.INTERVAL_DAY) {
+        if (value == AlarmManager.INTERVAL_DAY * 7) {
             settingsValue = 0;
-        } else if (value == AlarmManager.INTERVAL_HALF_DAY || value == 0) {
+        } else if (value == AlarmManager.INTERVAL_DAY) {
             settingsValue = 1;
-        } else if (value == AlarmManager.INTERVAL_HOUR) {
+        } else if (value == AlarmManager.INTERVAL_HALF_DAY) {
             settingsValue = 2;
         } else {
             settingsValue = 3;
